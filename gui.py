d6deecf44552fdf707cc5acf4cd22f3a663595a8
lambleyh <108937761+lambleyh@users.noreply.github.com>
2023-05-18 16:23:23 +0100

    Implemented gui "Edit" button

--- gui.py
+++ gui.py
@@ -1,21 +1,42 @@
 import functions as fn
 import PySimpleGUI as sg
 
+todos = fn.read_todos()
+todos = [item.capitalize() for item in todos]
+
 label = sg.Text("Enter a to-do item: ")
 input_box = sg.InputText(tooltip = "Enter to-do", key="todo", do_not_clear=False)
 add_botton = sg.Button("Add")
+list_box = sg.Listbox(values=todos, key="todolist", 
+                      enable_events=True, size=[40,10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window('To-Do App',
-                    layout=[[label], [input_box, add_botton]], 
+                    layout=[[label], [input_box, add_botton],[list_box, edit_button]], 
                     font=('Calibri',10))
 
 while True:
     event, values = window.read()
     match event:
         case "Add":
-            todos = fn.read_todos()
-            todos.append(values['todo'] + '\n')
-            fn.write_todos(todos)
+            if len(values['todo']) > 0:
+                todos.append(values['todo'] + '\n')
+                fn.write_todos(todos)
+                window['todolist'].update(values=todos)
+        case "Edit":
+            try:
+                todo = values['todolist'][0]
+                new_todo = values['todo'].capitalize() + '\n'
+
+                index = todos.index(todo)
+                todos[index] = new_todo
+                fn.write_todos(todos)
+
+                window['todolist'].update(values=todos)
+            except IndexError:
+                continue
+        case "todolist":
+            window['todo'].update(value=values['todolist'][0])
         case sg.WIN_CLOSED:
             break
 window.close()
