62683bf96baeb34e40b6ca34e86dded04eb8a703
lambleyh <108937761+lambleyh@users.noreply.github.com>
2023-05-31 14:14:45 +0100

    Finish gui and generate exe

--- gui.py
+++ gui.py
@@ -1,16 +1,23 @@
 import functions as fn
 import PySimpleGUI as sg
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt","w") as file:
+        pass
+
+sg.theme("BlueMono")
 
 todos = fn.read_todos()
 todos = [item.capitalize() for item in todos]
 
 label = sg.Text("Enter a to-do item: ")
 input_box = sg.InputText(tooltip = "Enter to-do", key="todo", do_not_clear=False, size=[40,1])
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", size=10)
 list_box = sg.Listbox(values=todos, key="todolist", 
                       enable_events=True, size=[40,10])
-edit_button = sg.Button("Edit")
-remove_button = sg.Button("Remove")
+edit_button = sg.Button("Edit", size=10)
+remove_button = sg.Button("Remove", size=10)
 
 window = sg.Window('To-Do App',
                     layout=[[sg.Column([[label], [input_box],[list_box]]),
@@ -39,6 +46,8 @@ while True:
 
                 window['todolist'].update(values=todos)
             except IndexError:
+                sg.popup("Please select an item to edit first!",
+                         font=('Calibri',10))
                 continue
         case "Remove":
             try:
@@ -50,6 +59,8 @@ while True:
                 window['todolist'].update(values=todos)
                 window['todo'].set_focus()
             except IndexError:
+                sg.popup("Please select an item to remove first!",
+                         font=('Calibri',10))
                 continue
         case "todolist":
             window['todo'].update(value=values['todolist'][0].strip('\n'))
