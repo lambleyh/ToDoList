ce6ac3c7e64f9b16ef2f7a600fa6ef9501f28d76
lambleyh <108937761+lambleyh@users.noreply.github.com>
2023-06-06 16:47:49 +0100

    Checkbox delete todo function added

--- web.py
+++ web.py
@@ -15,11 +15,16 @@ st.title("My Todo App")
 st.subheader("Welcome to my to-do list app!")
 st.write("Lorem ipsum")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        fn.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label = "input", placeholder = "Add a new todo...", 
               label_visibility = "collapsed", on_change = add_todo, 
               key = 'new_todo')
 
-st.session_state
+#st.session_state
